digitalregion/vbc-explorer

Changed location of config to root; Started logic to sync whole chain after restart.

routes/web3relay.js
@@ -51,7 +51,7 @@ if (typeof web3 !== "undefined") {
 if (web3.isConnected())
   console.log("Web3 connection established");
 else
-  throw "No connection, if your using a remote node please update the host in: ./routes/web3relay.js";
+  throw "No connection, please specify web3host in conig.json";
 var newBlocks = web3.eth.filter("latest");
 var newTxs = web3.eth.filter("pending");
 exports.data = function(req, res){

pyskell/explorer

routes/web3relay.js
@@ -51,7 +51,7 @@ if (typeof web3 !== "undefined") {
 if (web3.isConnected())
   console.log("Web3 connection established");
 else
-  throw "No connection, if your using a remote node please update the host in: ./routes/web3relay.js";
+  throw "No connection, please specify web3host in config.json";
 var newBlocks = web3.eth.filter("latest");
 var newTxs = web3.eth.filter("pending");
 exports.data = function(req, res){